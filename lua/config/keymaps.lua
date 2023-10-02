9eeb8e70a643ab5a5fef28a841643cc5410e29ba
yuzhang9804 <yuzhang9804@gmail.com>
2023-10-02 13:49:08 +0800

    chore: init

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+
+local opt = vim.opt
+opt.wrap = true
+
+opt.spelllang = "en,cjk"
+opt.spelloptions = "camel"
+opt.scrolloff = 5
+opt.indentexpr = ""
+opt.foldmethod = "indent"
+opt.foldlevel = 99
+opt.foldenable = true
+opt.foldlevelstart = 99
